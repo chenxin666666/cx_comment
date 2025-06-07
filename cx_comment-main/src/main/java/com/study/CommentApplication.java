70385d8f099f38f08e12d1bc2066b18a8c9ad927
里予 <1171064090@qq.com>
2025-06-07 12:19:29 +0800

    项目宠å重新初始化(以这个版本ç»为主)

---
+++ cx_comment-main/src/main/java/com/study/CommentApplication.java
@@ -0,0 +1,18 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类-评论项目
+ *
+ */
+@SpringBootApplication
+public class CommentApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CommentApplication.class,args);
+
+    }
+}
